eaea3a3a5e99f517087ba890134d583adf974479
Jake Crespo <jakeacrespo@gmail.com>
2020-05-18 10:42:02 -0500

    SpaceX - Capsules and Missions sample page

---
+++ src/pages/capsules.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const CapsulesPage = () => (
+  <Layout>
+    <SEO title="Capsules" />
+    <h1>CAPSULES</h1>
+  </Layout>
+)
+
+export default CapsulesPage
